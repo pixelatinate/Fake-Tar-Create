1024b2168d33e56bafc3d89c76aa0fd8dfe10874
Swasti Mishra <smishr11@vols.utk.edu>
2022-03-13 21:51:03 -0400

    Added Libraries
    Mostly reading the lab write up and thinking about how to set it up... didn't write a lot of code.

---
+++ src/tarc.c
@@ -0,0 +1,23 @@
+// Lab 5: tarc.c
+// This lab is will mimic the functionality of the program tar,  
+//      which bundles up files and directories into a single file.
+//      See http://web.eecs.utk.edu/~jplank/plank/classes/cs360/360/labs/Lab-5-Tarc/index.html
+//      for more information and lab specifications. 
+
+// COSC 360
+// Swasti Mishra 
+// Mar 21, 2022
+// James Plank 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "jrb.h"
+#include "dllist.h"
+
+int main(){
+	return 0 ;
+}
